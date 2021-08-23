1a68f4a5ad9ab5a386066d0bb014a809445a3ba3
vincijy <vincijy@foxmail.com>
2021-08-24 03:45:27 +0800

    feat: add message error when request failed

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { BASE_URL, TIMEOUT } from '@/config';
 import store from '@/store';
+import { message } from 'antd';
+
 
 // 基本配置
 const instance = axios.create({
@@ -30,13 +32,16 @@ instance.interceptors.response.use((res) => res.data, (err) => {
   if (err && err.response) {
     switch (err.response.status) {
       case 400:
-        console.log('请求错误');
+        message.error('请求错误');
         break;
       case 401:
-        console.log('未授权访问');
+        message.error('未授权访问');
+        break;
+      case 500:
+        message.error('服务端错误');
         break;
       default:
-        console.log('其他错误信息');
+        message.error('其他错误信息');
     }
   }
   return err;
