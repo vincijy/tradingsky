ee4b8dba06133b27d2d2ea3874bebd70177c9036
vincijy <vincijy@foxmail.com>
2021-09-26 01:41:50 +0800

    fix: text vertical align

--- src/pages/studio/char_head/style.ts
+++ src/pages/studio/char_head/style.ts
@@ -3,7 +3,6 @@ import * as Layout from '../../../config/layout';
 
 export const ChartTitleArea = styled.div `
     position: absolute;
-    // border: 1px solid rgb(226, 226, 226);
     top: ${Layout.CHART_HEADER_TOP}px;
     height: ${Layout.CHART_HEADER_HEIGHT}px;
     z-index: 1;
@@ -20,8 +19,8 @@ export const ChartTitleArea = styled.div `
         font-size: 0.8rem;
         display: inline-block;
         vertical-align: middle;
-        height: 100%;
-        /* line-height: 30px; */
+        height: ${Layout.CHART_HEADER_HEIGHT}px;;
+        line-height: ${Layout.CHART_HEADER_HEIGHT}px;;
         margin-left: 4px;
     }
 `;
