99308267b349842ea7babf28ef52f43236a2a487
vincijy <vincijy@foxmail.com>
2021-09-26 19:37:51 +0800

    feat: import colorAxis module

--- src/components/chart/index.tsx
+++ src/components/chart/index.tsx
@@ -4,7 +4,7 @@ import initExport from 'highcharts/modules/exporting';
 import HSIndicators from 'highcharts/indicators/indicators';
 import { globalOptions } from '@/indices/chart_common/gloabl';
 import offlineExporting from 'highcharts/modules/offline-exporting';
-
+import colorAxis from 'highcharts/modules/coloraxis';
 let isSetOption = false;
 
 /**
@@ -17,6 +17,7 @@ export const getHighCharts = function():typeof Highcharts {
     Highcharts.setOptions(globalOptions);
     initExport(Highcharts);
     offlineExporting(Highcharts);
+    colorAxis(Highcharts);
     HSIndicators(Highcharts);
     isSetOption = true;
   }
