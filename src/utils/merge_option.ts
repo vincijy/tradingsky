f0f5087059f7b6d6807b652d61b3c0e5109311cb
vincijy <vincijy@foxmail.com>
2021-09-27 00:56:46 +0800

    feat: replace bg img

--- src/utils/merge_option.ts
+++ src/utils/merge_option.ts
@@ -1,10 +1,17 @@
 import { commonOptions } from '@/indices/chart_common';
 import { mergeWith } from 'lodash';
-export function mergeOption(commonOption:any, privateOption:any) {
-  // const newOp = mergeWith({}, commonOption, privateOption);
-  // return { ...newOp };
-  return { ...commonOptions, ...privateOption };
-};
 
+function replaceByPrivate(newOption:any, privateOption:any) {
+  // 背景图片替换, TODO, 写一个更加通用的
+  if (privateOption.chart && privateOption.chart.plotBackgroundImage) {
+    newOption.chart.plotBackgroundImage = privateOption.chart.plotBackgroundImage;
+  }
+}
+
+export function mergeOption(commonOption:any, privateOption:any) {
+  const newOption = { ...commonOptions, ...privateOption };
+  replaceByPrivate(newOption, privateOption);
+  return newOption;
+};
 
 (window as any).mergeOption = mergeOption;
