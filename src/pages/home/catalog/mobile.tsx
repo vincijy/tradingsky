0e9ac36298d80470d50d866476f98f3ddc4e729d
张洛竹 <9567819+zhang-luozhu@user.noreply.gitee.com>
2021-10-01 12:17:09 +0000

    fix：introduce

--- src/pages/home/catalog/mobile.tsx
+++ src/pages/home/catalog/mobile.tsx
@@ -7,17 +7,17 @@ import { CatalogWrapper } from './mobile_style';
 export default memo(function LSHomeCatalog() {
   const content = (
     <div className='catalog-container'>
-      <h2 className='head-text'>智能化的链上数据分析可视化平台</h2>
+      <h2 className='head-text'>智能化的链上数据分析平台</h2>
       <div className='catalog-img-area' />
       <ul className='catalog-text-area'>
         <li className='small-text'>更智能, 更易于使用</li>
         <li className='desc'>精选 40+ 指标, 100+图表, 种类丰富, 分类详细</li>
-        <li className='desc'>基于大数据分析技术高频采集链上数据</li>
-        <li className='desc'>基于机器学习的智能数据批注, 提供人性化解读</li>
-        <li className='desc'>多币种切换, 满足不同用户数据需求</li>
-        <li className='desc'>易于理解的指标用法与解释</li>
-        <li className='desc'>10000+ 用户, 区块链交易者、投资者、研究人员的理性选择</li>
-        <li className='desc'>资深区块链研究人员,数据分析师,开发者为产品提供持续迭代</li>
+        <li className='desc'>基于机器学习及各类算法标记链上地址标签2000万+</li>
+        <li className='desc'>2000+ 付费用户, 区块链投资者、研究人员的理性选择</li>
+        <li className='desc'>部署多个全节点，保证数据的稳定性和实时性</li>
+        <li className='desc'>专业化的指标原理介绍，让您获取最前沿的数据解读</li>
+        <li className='desc'>支持多币种切换, 满足用户的多元需求</li>
+        <li className='desc'>资深区块链研究人员,数据科学家,开发者为产品提供持续迭代</li>
       </ul>
     </div>
   );
--- src/pages/home/catalog/pc.tsx
+++ src/pages/home/catalog/pc.tsx
@@ -10,15 +10,15 @@ export default memo(function LSHomeCatalog() {
     <div className='catalog-container'>
       <div className='catalog-img-area' />
       <div className='catalog-text-area'>
-        <h2 className='head-text'>智能化的链上数据分析可视化平台</h2>
+        <h2 className='head-text'>智能化的链上数据分析平台</h2>
         <p className='small-text'>更智能, 更易于使用</p>
         <p className='desc'>精选 40+ 指标, 100+图表, 种类丰富, 分类详细</p>
-        <p className='desc'>基于大数据分析技术高频采集链上数据</p>
-        <p className='desc'>基于机器学习的智能数据批注, 提供人性化解读</p>
-        <p className='desc'>多币种切换, 满足不同用户数据需求</p>
-        <p className='desc'>易于理解的指标用法与解释</p>
-        <p className='desc'>10000+ 用户, 区块链交易者、投资者、研究人员的理性选择</p>
-        <p className='desc'>资深区块链研究人员,数据分析师,开发者为产品提供持续迭代</p>
+        <p className='desc'>基于机器学习及各类算法标记链上地址标签2000万+</p>
+        <p className='desc'>2000+ 付费用户, 区块链投资者、研究人员的理性选择</p>
+        <p className='desc'>部署多个全节点，保证数据的稳定性和实时性</p>
+        <p className='desc'>专业化的指标原理介绍，让您获取最前沿的数据解读</p>
+        <p className='desc'>支持多币种切换, 满足用户的多元需求</p>
+        <p className='desc'>资深区块链研究人员,数据科学家,开发者为产品提供持续迭代</p>
       </div>
     </div>
   );
