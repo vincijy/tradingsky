28c2e707b18177fcfab50f52efe65cc5a53d117a
vincijy <vincijy@foxmail.com>
2021-10-01 23:53:20 +0800

    fix: catalog img

--- src/oss.ts
+++ src/oss.ts
@@ -1,8 +1,6 @@
 import homeMain from '@/assets/img/home_main.png';
 import chart_0 from '@/assets/img/chart_0-min.jpg';
 import chart_1 from '@/assets/img/chart_1-min.png';
-
-import catalog_chart from '@/assets/img/catalog_chart-min.jpg';
 import qrcode from '@/assets/img/qrcode-min.jpg';
 import restriction from '@/assets/img/restriction-min.jpg';
 
@@ -10,7 +8,7 @@ export const ossImgs = {
   homeMain,
   chart_0,
   chart_1,
-  catalog_chart,
+  catalog_chart: chart_1,
   qrcode,
   restriction,
 };
