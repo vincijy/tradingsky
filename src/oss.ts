7e0bd1ef13924ca83c49848f88048e1affeac19b
vincijy <vincijy@foxmail.com>
2021-10-01 23:51:34 +0800

    feat: another img of home page

--- src/oss.ts
+++ src/oss.ts
@@ -1,5 +1,7 @@
 import homeMain from '@/assets/img/home_main.png';
 import chart_0 from '@/assets/img/chart_0-min.jpg';
+import chart_1 from '@/assets/img/chart_1-min.png';
+
 import catalog_chart from '@/assets/img/catalog_chart-min.jpg';
 import qrcode from '@/assets/img/qrcode-min.jpg';
 import restriction from '@/assets/img/restriction-min.jpg';
@@ -7,6 +9,7 @@ import restriction from '@/assets/img/restriction-min.jpg';
 export const ossImgs = {
   homeMain,
   chart_0,
+  chart_1,
   catalog_chart,
   qrcode,
   restriction,
