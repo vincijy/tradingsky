d031dcbe27b686506a7a4225642c96cb8b0d96c4
vincijy <vincijy@foxmail.com>
2021-10-01 20:42:15 +0800

    fix: xAxis label formater

--- src/indices/chart_common/x_axis.ts
+++ src/indices/chart_common/x_axis.ts
@@ -9,14 +9,14 @@ export const xAxis = {
 
       // TODO: 使用统一的时间格式化工具函数, 避免一些容易搞错的问题
       const date = new Date(value);
-      const selected = (this as any).chart.rangeSelector.selected;
 
-      // TODO: magic number
-      if (selected === undefined || selected === 6) {
+      // TODO: 搞清楚这几个值的含义
+      const extremes = (this as any).chart.axes[0].getExtremes() as { min:number, max:number, userMin:number, userMax:number, dataMax:number, dataMin:number };
+      if ((extremes.dataMax) === (extremes.max)) {
         return `${date.getFullYear()}年`;
       }
 
-      return `${date.getMonth() + 1}月 ${date.getDate()}日`;
+      return `${date.getMonth() + 1}月${date.getDate()}日`;
     },
   },
   events: {
