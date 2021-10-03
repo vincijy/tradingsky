a1c238cd1203a4c58d050c1e91bf4c50ea488a7b
vincijy <vincijy@foxmail.com>
2021-10-03 23:32:43 +0800

    fix: proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use('/api', createProxyMiddleware({
-    target: 'http://localhost:8081',
+    target: 'http://121.43.176.46:8081',
     changeOrigin: true,
   }));
 
