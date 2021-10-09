a3617f1999ae1d869c7f0e4a7c625544c4d3e459
vincijy <vincijy@foxmail.com>
2021-10-09 21:45:20 +0800

    fix: default indice shown

--- src/config/init_state.ts
+++ src/config/init_state.ts
@@ -1,9 +1,8 @@
 import { menus } from '@/indices';
 import { getUrlParams, findMenuByIndex } from '@/utils/url';
 import { mergeOption } from '@/utils/merge_option';
-import { chartBtc as initialPrivateOption } from '@/indices/6_sentiment/sopr/chart_btc';
+import { chart as initialPrivateOption } from '@/indices/5_evaluate_indice/arh999/chart';
 import { commonOptions } from '@/indices/chart_common';
-import store from '@/store';
 
 /**
  * 如果能从URL中读取到指标的话初始状态就设置成改指标对应菜单, 否则使用默认的sopf作为菜单
@@ -21,8 +20,8 @@ export function getAppInitState() {
 
   const appInitState = {
     chartOptions: mergeOption(commonOptions, chartOptions || initialPrivateOption),
-    menu: menu || menus[7],
-    subMenu: subMenu || menus[7].subMenus[0],
+    menu: menu || menus[6],
+    subMenu: subMenu || menus[6].subMenus[0],
     dataAsset: dataAssetStr ? dataAssetStr : 'btc',
   };
 
