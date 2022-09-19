8c679a7f0abbd3e3c88a33fc501f8e585b94a9d0
vincijy <vincijy@foxmail.com>
2022-09-20 02:12:40 +0800

    feat: style issue

--- src/style.ts
+++ src/style.ts
@@ -3,6 +3,9 @@ import { createGlobalStyle } from 'styled-components';
 import rusty from '@assets/ttf/rusty.jpg';
 
 export const FontStyle = createGlobalStyle `
+ul {
+  list-style-type: none;
+}
 @font-face
 {
   font-family: "Rusty - Demo";
